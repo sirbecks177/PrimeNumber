45a36b2f713841809b4ac157023c956ec6d74551
David Omondiagbe <103688535+sirbecks177@users.noreply.github.com>
2022-05-23 12:27:07 +0300

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,26 +4,32 @@ import java.util.Scanner;
 import java.util.Scanner;
 public class PrimeNumber {
 public static void main(String[] args) {
-Scanner s = new Scanner(System.in);
+Scanner sc = new Scanner(System.in);
 System.out.print("Please give a number> ");
-int n = s.nextInt();
-if (isPrime(n)) {
+int n = sc.nextInt();
+boolean isPrime = checkPrime(n);
+if (isPrime) {
 System.out.println(n + " is prime number and factors are");
 } else {
 System.out.println(n + " is composite number and factors are");
 }
 }
-public static boolean isPrime(int n) {
+public static boolean checkPrime(int n) {
+  
+  boolean isPrime = true;
 if (n <= 1) {
-return false;
+isPrime=false;
+return isPrime;
 }
-for (int i = 2; i < Math.sqrt(n); i++) {
+for (int i = 2; i < n/2; i++) {
 if (n % i == 0) {
 
-return false;
+isPrime=false;
+return isPrime;
 
 }
 }
-return true;
+
+return isPrime;
 }
 }
